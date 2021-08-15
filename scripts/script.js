9ca2fb99c7daca4117420f7e990630782cb09173
gashnick <88553941+gashnick@users.noreply.github.com>
2021-08-15 19:36:03 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
 
+const email = document.getElementById("Email");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please provide a valid email address");
+  } else {
+    email.setCustomValidity("");
+  }
+});
